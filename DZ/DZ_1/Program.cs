be8768da13b728bb7735590840aa1b2670967e91
Danil <2461596@mail.ru>
2022-10-16 16:20:14 +0500

    DZ_1

---
+++ DZ/DZ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ВВедите число a");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ВВедите число b");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
