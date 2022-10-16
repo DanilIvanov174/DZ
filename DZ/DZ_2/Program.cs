b043d8ff408054cda2a33c2117afbd07d23066d7
Danil <2461596@mail.ru>
2022-10-16 17:06:35 +0500

    DZ_2

---
+++ DZ/DZ_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("ВВедите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ВВедите второе число");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("ВВедите третье число");
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("max = " + max);
