7e32c82557a58ba17d6871a87a9b0d0237e65002
Danil <2461596@mail.ru>
2022-10-16 17:21:21 +0500

    DZ_3

---
+++ DZ/DZ_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите число");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}
