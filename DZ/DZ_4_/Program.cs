770e1b352d68c2ea30c9189606e8cf374674e310
Danil <2461596@mail.ru>
2022-10-16 17:55:55 +0500

    DZ_4

---
+++ DZ/DZ_4_/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ВВедите число");
+int a = int.Parse(Console.ReadLine()!);
+int b = 2;
+if(a>0)
+{
+    while (b < a)
+    {
+        Console.WriteLine(b + " ");
+        b = b + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Необходимо ввести положительное число");
+}
