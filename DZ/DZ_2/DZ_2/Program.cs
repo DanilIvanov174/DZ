e39fa9cceb0f3a40fb3825ef61f8cc0e329c278e
Danil <2461596@mail.ru>
2022-10-18 21:17:30 +0500

    Лекция_2_ДЗ_2

---
+++ DZ/DZ_2/DZ_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int ThirdDigit()
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    if (number <= 1000 && number >= 100)
+    {
+        number = number % 10;
+    }
+
+    else
+    {
+        Console.Write("Третьей цифры нет в числе ");
+    }
+    return number;
+}
+
+Console.WriteLine(ThirdDigit());
