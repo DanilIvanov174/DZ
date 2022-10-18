3c208d5c65085b3ee6442b5a34c281a472e19e6c
Danil <2461596@mail.ru>
2022-10-18 22:16:32 +0500

    Лекция_2_ДЗ_3

---
+++ DZ/DZ_2/DZ_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int number = int.Parse(Console.ReadLine()!);
+
+void Weekend()
+{
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Число не в диапазоне от 1 до 7");
+}
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+}
+
+Weekend();
