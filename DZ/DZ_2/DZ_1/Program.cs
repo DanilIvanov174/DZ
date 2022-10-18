0aa484805d8b485adacff545deed927e06a0fa51
Danil <2461596@mail.ru>
2022-10-18 19:40:13 +0500

    Лекция_2_ДЗ_1

---
+++ DZ/DZ_2/DZ_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+int SecondDigit()
+{
+    int secondDigit = number / 10 % 10;
+    return secondDigit;
+}
+
+Console.WriteLine(SecondDigit());
