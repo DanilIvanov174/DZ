6b7ac50bfc295c245c6542b1951e1456657de6b1
Danil_Ivanov <2461596@mail.ru>
2022-11-18 18:56:01 +0300

    Zadacha_41

---
+++ DZ/DZ_6/zadacha_41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Программа которая, считает сколько чисел больше 0 ввёл пользователь.");
+
+Console.WriteLine("Введите количество чисел ");
+int lenght = int.Parse(Console.ReadLine()!);
+int[] array = new int[lenght];
+
+Console.WriteLine("Введите числа через 'Enter' ");
+PrintArray(ReadArray(array));
+int rezult = CountGreaterThanZero(array);
+Console.WriteLine($"Количество чисел больше нуля = {rezult}");
+
+
+int[] ReadArray(int[] array)  // Функция ввода массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)  // Функция вывода массива
+{
+    Console.WriteLine();
+    Console.WriteLine("Вы ввели числа ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountGreaterThanZero (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+            count++;
+    }
+    return count;
+}
