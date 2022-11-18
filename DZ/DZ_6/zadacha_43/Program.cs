68ba36bda322a717994aeb923409f1300e4946ad
Danil_Ivanov <2461596@mail.ru>
2022-11-18 19:08:43 +0300

    Zadacha_43

---
+++ DZ/DZ_6/zadacha_43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введите значение b2");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double x = Math.Round(((-b2 + b1) / (-k1 + k2)), 1);
+double y = Math.Round((k2 * x + b2), 1);
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x} Y: {y}");
